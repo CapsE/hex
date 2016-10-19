baa86cb709d55303e75b469613675227f80fe0b2
Lars <larskrafft@yahoo.de>
2016-10-19 23:14:37 +0200

    TST-2: Feature Branches funktionieren an sich.

--- hex.js
+++ hex.js
@@ -6,6 +6,8 @@ var JiraClient = require('jira-connector');
 
 
 var ENV = require('./hex-config');
+var CMD = process.argv[2] || "";
+var VALUE = process.argv[3] || "";
 
 var Git = require('simple-git')();
 var program = require('commander');
@@ -20,29 +22,46 @@ var jira = new JiraClient( {
 
 program
     .version('0.0.1')
-    .option('-f, --feature [name]', 'Create new feature')
-    .arguments('do [env]')
-    .action(function (cmd, env) {
-        cmdValue = cmd;
-        envValue = env;
-        console.log("Executing", cmd);
-    });
+    .option('-f, --feature [name]', 'Create new feature');
 
 program.parse(process.argv);
 
 if(program.feature){
+    feature(program.feature);
+}
+
+function feature(id) {
     //console.log("Creating feature", ENV.jira +  program.feature);
-    console.log("Looking for ", ENV.project +  program.feature);
+    console.log("Looking for ", ENV.project +  id);
     jira.issue.getIssue({
-        issueKey: ENV.project +  program.feature
+        issueKey: ENV.project +  id
     }, function(error, issue) {
         if(error){
             console.log("Error: ", error);
         }else{
-            Git.checkout("-b" + ENV.project +  program.feature);
+            Git.checkout("-b" + ENV.project +  id);
             Git.branch("--edit-description" + issue.fields.summary);
-            console.log("Created Branch for: ", ENV.project +  program.feature + ":" + issue.fields.summary);
+            console.log("Created Branch for: ", ENV.project +  id + ":" + issue.fields.summary);
         }
     });
 }
 
+function checkout(id){
+    var ex = /[0-9]+/.test(id);
+    console.log(ex);
+    if(ex){
+        Git.checkout(ENV.project +  id);
+    }else{
+        Git.checkout(id);
+    }
+
+}
+
+switch(CMD) {
+    case "f":
+        feature(VALUE);
+        break;
+    case "go":
+        checkout(VALUE);
+        break;
+}
