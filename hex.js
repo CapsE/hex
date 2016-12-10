2f155022168dc84c0e6dc0133640708c22b52cb4
Lars <larskrafft@yahoo.de>
2016-12-10 10:36:49 +0100

    DEV: Test commit

--- hex.js
+++ hex.js
@@ -50,29 +50,28 @@ try{
 }
 
 function feature(id) {
-    Git.command("checkout", "PROD").then(function(){
-        if(program.nojira){
-            var ex = /[0-9]+/.test(id);
-            if(ex){
-                Git.checkout("-b" + ENV.project +  id);
-            }else{
-                Git.checkout("-b" + id);
-            }
+    checkout(ENV["prod"]);
+    if(program.nojira){
+        var ex = /[0-9]+/.test(id);
+        if(ex){
+            Git.checkout("-b" + ENV.project +  id);
         }else{
-            console.log("Looking for ", ENV.project +  id);
-            jira.issue.getIssue({
-                issueKey: ENV.project +  id
-            }, function(error, issue) {
-                if(error){
-                    console.log("Error: ", error);
-                }else{
-                    Git.checkout("-b" + ENV.project +  id);
-                    Git.branch("--edit-description" + issue.fields.summary);
-                    console.log("Created Branch for: ", ENV.project +  id + ":" + issue.fields.summary);
-                }
-            });
+            Git.checkout("-b" + id);
         }
-    });
+    }else{
+        console.log("Looking for ", ENV.project +  id);
+        jira.issue.getIssue({
+            issueKey: ENV.project +  id
+        }, function(error, issue) {
+            if(error){
+                console.log("Error: ", error);
+            }else{
+                Git.checkout("-b" + ENV.project +  id);
+                Git.branch("--edit-description" + issue.fields.summary);
+                console.log("Created Branch for: ", ENV.project +  id + ":" + issue.fields.summary);
+            }
+        });
+    }
 }
 
 function checkout(id){
